1ed51ad42c6dd8ae1d50aeded57eba008d376449
Nike1421 <ommiemo54@gmail.com>
2023-06-24 00:03:43 +0530

    Changed Paths To Relative Paths

--- sqlGameBackend/backend.py
+++ sqlGameBackend/backend.py
@@ -1,16 +1,17 @@
-import dataframe_image as dfi
-from flask import Flask, jsonify, request
-from flask_restful import Resource, Api
-from flask_cors import CORS
+import os
 import pandas as pd
 import sqlite3 as sql
-import os
+from flask_cors import CORS
+import dataframe_image as dfi
+from flask_restful import Resource, Api
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 cors = CORS(app)
 api = Api(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 con = sql.connect(
-    r'C:\Users\voras\Desktop\SQL-murder-Mystery\sqlGameBackend\sql-murder-mystery.db', check_same_thread=False)
+    os.path.abspath('./sqlGameBackend/sql-murder-mystery.db'), check_same_thread=False)
 queries = [
     '''
 SELECT *
@@ -72,19 +73,19 @@ class GameBackend(Resource):
             level = request.form['level']
             queryResult = pd.read_sql_query(query, con)
             result = queryResult.equals(output[int(level)])
-            if os.path.isfile(r'C:\Users\voras\Desktop\SQL-murder-Mystery\sqlgame\src\output.png'):
+            if os.path.isfile(os.path.abspath('./sqlgame/src/output.png')):
                 os.remove(
-                    r'C:\Users\voras\Desktop\SQL-murder-Mystery\sqlgame\src\output.png')
-            dfi.export(queryResult, 'C:/Users/voras/Desktop/SQL-murder-Mystery/sqlgame/src/output.png',
+                    os.path.abspath('./sqlgame/src/output.png'))
+            dfi.export(queryResult, os.path.abspath('./sqlgame/src/output.png'),
                        table_conversion='matplotlib')
             response = jsonify({'data': result})
             response.status_code = 200
             return response
         except Exception as e:
             print(e)
-            if os.path.isfile(r'C:\Users\voras\Desktop\SQL-murder-Mystery\sqlgame\src\output.png'):
+            if os.path.isfile(os.path.abspath('./sqlgame/src/output.png')):
                 os.remove(
-                    r'C:\Users\voras\Desktop\SQL-murder-Mystery\sqlgame\src\output.png')
+                    os.path.abspath('./sqlgame/src/output.png'))
             response = jsonify({'error': 'Its a syntax error'})
             response.status_code = 400
             return response
