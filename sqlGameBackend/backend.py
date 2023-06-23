49b502c2f7fa8d5be90c44703898391fcd7ceded
Nike1421 <ommiemo54@gmail.com>
2023-06-24 00:21:55 +0530

    Changed path handler from os.path to pathlib

--- sqlGameBackend/backend.py
+++ sqlGameBackend/backend.py
@@ -1,17 +1,20 @@
 import os
 import pandas as pd
 import sqlite3 as sql
+import pathlib
 from flask_cors import CORS
 import dataframe_image as dfi
 from flask_restful import Resource, Api
 from flask import Flask, jsonify, request
+
+import file_path_constants as FPC
 
 app = Flask(__name__)
 cors = CORS(app)
 api = Api(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 con = sql.connect(
-    os.path.abspath('./sqlGameBackend/sql-murder-mystery.db'), check_same_thread=False)
+    FPC.db_path, check_same_thread=False)
 queries = [
     '''
 SELECT *
@@ -73,19 +76,19 @@ class GameBackend(Resource):
             level = request.form['level']
             queryResult = pd.read_sql_query(query, con)
             result = queryResult.equals(output[int(level)])
-            if os.path.isfile(os.path.abspath('./sqlgame/src/output.png')):
+            if FPC.output_image_path.exists():
                 os.remove(
-                    os.path.abspath('./sqlgame/src/output.png'))
-            dfi.export(queryResult, os.path.abspath('./sqlgame/src/output.png'),
+                    FPC.output_image_path)
+            dfi.export(queryResult, FPC.output_image_path,
                        table_conversion='matplotlib')
             response = jsonify({'data': result})
             response.status_code = 200
             return response
         except Exception as e:
             print(e)
-            if os.path.isfile(os.path.abspath('./sqlgame/src/output.png')):
+            if FPC.output_image_path.exists():
                 os.remove(
-                    os.path.abspath('./sqlgame/src/output.png'))
+                    FPC.output_image_path)
             response = jsonify({'error': 'Its a syntax error'})
             response.status_code = 400
             return response
---
+++ sqlGameBackend/file_path_constants.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+# Path to Database
+db_path = Path("sqlGameBackend/sql-murder-mystery.db")
+
+# Path to Output Image
+output_image_path =Path("sqlgame/src/output.png")
