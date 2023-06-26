06476cfa2176671366fe23b0b8c66e9a3d3445bf
Nike1421 <ommiemo54@gmail.com>
2023-06-26 19:35:03 +0530

    Modify Questions for proper fitting

--- sql-game-python/game_constants.py
+++ sql-game-python/game_constants.py
@@ -45,15 +45,15 @@ WHERE person_id = "51739" OR person_id = "67318";
 ]
 
 questions = [
-    "There's been a Murder in SQL City!  A crime has taken place and I need your help. I have given you the crime scene report, but you somehow lost it. You vaguely remember that murder occurred sometime on Jan 15, 2018 and that it took place in SQL City. Start by retrieving the corresponding crime scene report from the police department’s database.",
-    "Security footage shows that there were two witnesses. The first witness named Morty lives at the last house on \"Northwestern Dr \". Let's, look for the details of first witness",
-    "The second witness, named Annabel, lives somewhere on \"Franklin Ave\" \n Let's, look for the details of second witness",
-    "lets view the interview of both the witnesses taken after the murder.",
-    "So, we got 2 clues-  \n Killer is a man and a member of the gym with a status of gold and having a membership no. starting with 48Z and left in a car with a no. plate of H42W and He was working out in the gym on 9th of Jan. Check the gym database with above details",
-    "Two member's found and their membership id. Now, let's check the car details by the above details",
-    "Two male with a plate no. containg H42W. Check personal details of both the males from the above query",
-    "Lets check which of this two are a member of the gym?",
-    "Congrats, you found the murderer - Jeremy Bowers. Both the membership id and status also matches as per the information we found earlier.",
+    "There's been a Murder in SQL City!\nA crime has taken place and I need\nyour help. I have given you the crime\nscene report, but you somehow lost it.\nYou vaguely remember that the murder\noccurred sometime on Jan 15, 2018\nand that it took place in SQL City.\nStart by retrieving the corresponding\ncrime scene report from the police\ndepartment's database.",
+    "The security footage shows\nthat there were two witnesses.\nThe first witness, Mr. Morty lives in\nthe last house on\"Northwestern\nDr\". Let us look for the details\nof the first witness.",
+    "The second witness,\nAnnabel, lives somewhere\n on \"Franklin Ave\". Now,\nlet's get her details.",
+    "Now, let's take a\nlook at the interviews\nof the witnesses taken\nafter the murder...",
+    "All right, there are two clues.\nThe killer is a male and holds\na Gold Membership in the Gym.\nHis ID starts with \'48Z\' and he\nwas working out in the gym on\nJan. 9. He left in a car with a\nnumber plate H42W. Check the\ngym database with these details.",
+    "There are two gym members\nwith matching membership IDs.\nNow, let's check the car details\nsimilarly...",
+    "Good job! There are two\nmales having cars with plate\nnumbers containing \'H42W\'.\nNow, check the personal\ndetails of them both...",
+    "Now, of these two,\nwho is a member of the Gym?\nFind that out please!",
+    "Congratulations, you found\nthe murderer - Jeremy Bowers.\nBoth the membership ID and status\nmatch as per the information\nwe found earlier.",
 ]
 
 hints = [
