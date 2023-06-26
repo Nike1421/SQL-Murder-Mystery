4abd607d988596094846c1a7a3cc8e913193c64a
Nike1421 <ommiemo54@gmail.com>
2023-06-26 19:35:26 +0530

    Changed Score Mechanics

--- sql-game-python/scoreboard.py
+++ sql-game-python/scoreboard.py
@@ -9,4 +9,10 @@ class Scoreboard:
 
     def level_up(self):
         self.level += 1
-        self.score += 20
+        self.score += 100
+
+    def wrong_answer(self):
+        self.score -= 20
+
+    def asked_hint(self):
+        self.score -= 10
